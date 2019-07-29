fff2df8de820510dfb9e7443357d3f8220a17f06
Michael Brown <mbrown@fensystems.co.uk>
2019-07-29 19:08:56 +0100

    Use setuptools instead of OpenStack pbr
    OpenStacks pbr is highly opinionated about the packaging process, and
    its opinions conflict with the opinions of the tito RPM builder.
    Remove the pbr-based setup.cfg and use plain setuptools instead.
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- setup.py
+++ setup.py
@@ -1,29 +1,41 @@
-# Copyright (c) 2019 Unipart Digital
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python3
 
-# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
-import setuptools
+"""Setup script"""
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
+from setuptools import setup
 
-setuptools.setup(
-    setup_requires=['pbr>=2.0.0'],
-    pbr=True)
+setup(
+    name="neutron-dnscurrent-plugin",
+    description="Use dns_current_name as machine hostname",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Unipart Digital",
+    author_email="sysadmins@unipart.io",
+    url="https://github.com/unipartdigital/neutron-dnscurrent-plugin",
+    license="Apache",
+    version="0.0.1",
+    classifiers=[
+        "Environment :: OpenStack",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=[
+        'neutron_dnscurrent',
+    ],
+    install_requires=[
+        'neutron',
+    ],
+    entry_points={
+        'neutron.ml2.extension_drivers': [
+            'dnscurrent=neutron_dnscurrent:DNSCurrentExtensionDriver',
+        ],
+    },
+)
